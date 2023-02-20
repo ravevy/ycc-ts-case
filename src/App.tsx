ac4a2ecf0e9caa618e66653d9540dc060a915d50
ravevy <beyzbaser@gmail.com>
2023-02-20 19:16:08 +0300

    prettier düzeltmesi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface PostType {
@@ -10,42 +10,62 @@ interface PostType {
 }
 
 export default function App() {
- const [data, setData] = useState<PostType[]>([])
- const [input, setInput] = useState<string>()
- const [list, setList] = useState<PostType[]>(data)
- 
- useEffect(() => {
-  axios
-  .get<PostType[]>("https://jsonplaceholder.typicode.com/posts")
-  .then(res => {setData(res.data); setList(res.data)})
-  .catch((e)=>{console.log(e)})
- }, [])
+  const [data, setData] = useState<PostType[]>([]);
+  const [input, setInput] = useState<string>();
+  const [list, setList] = useState<PostType[]>(data);
 
- function handleClick(): void {
-  input === undefined ? setList(data) : setList(data.filter((i) => {return i.body.includes(input)}))
- }
+  useEffect(() => {
+    axios
+      .get<PostType[]>("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setData(res.data);
+        setList(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
- function handleKeyDown(e : React.KeyboardEvent<HTMLInputElement>): void{
-  if(e.key === "Enter")
-   handleClick()
- }
+  function handleClick(): void {
+    input === undefined
+      ? setList(data)
+      : setList(
+          data.filter((i) => {
+            return i.body.includes(input);
+          })
+        );
+  }
 
- function handleChange(e : React.ChangeEvent<HTMLInputElement>): void{
-  setInput(e.target.value)
- }
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>): void {
+    if (e.key === "Enter") handleClick();
+  }
 
-  return <> 
-    <div className="App">
-      <div className='search'>
-        <input type="text" onKeyDown={handleKeyDown} onChange={handleChange} placeholder="What are you looking for?"></input>
-        <button onClick={handleClick}>search</button>
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setInput(e.target.value);
+  }
+
+  return (
+    <>
+      <div className="App">
+        <div className="search">
+          <input
+            type="text"
+            onKeyDown={handleKeyDown}
+            onChange={handleChange}
+            placeholder="What are you looking for?"
+          ></input>
+          <button onClick={handleClick}>search</button>
+        </div>
+        <div className="list">
+          {data && (
+            <ul>
+              {list.map((post) => (
+                <li key={post.id}>{post.body}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      <div className='list'>
-       {data && <ul>
-          {list.map((post) => (
-          <li key={post.id}>{post.body}</li>))}
-        </ul>} 
-      </div>
-    </div>
-  </>
+    </>
+  );
 }
